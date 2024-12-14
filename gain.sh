1700c49a4dd089254fefa573f39d8afcb395949f
Corn Pop <63576755+cym31153@users.noreply.github.com>
2024-12-14 15:10:00 +0800

    Create gain.sh

---
+++ gain.sh
@@ -0,0 +1,11 @@
+NETWORK_PATH1='admin'
+NETWORK_PATH2='index'
+Advanced_Settings=$(cat <<-EOF
+ProxyRequests off
+ProxyPass "${NETWORK_PATH1}" "ws://services-${USER}.alwaysdata.net:8300${NETWORK_PATH1}"
+ProxyPassReverse "${NETWORK_PATH1}" "ws://services-${USER}.alwaysdata.net:8300${NETWORK_PATH1}"
+ProxyPass "${NETWORK_PATH2}" "ws://services-${USER}.alwaysdata.net:8400${NETWORK_PATH2}"
+ProxyPassReverse "${NETWORK_PATH2}" "ws://services-${USER}.alwaysdata.net:8400${NETWORK_PATH2}"
+ProxyPreserveHost On
+EOF
+)
